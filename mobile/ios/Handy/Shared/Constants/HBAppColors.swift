60ae3721409df8be7a1d2533029fb3061136e802
Grigori Jlavyan <jlavyan@gmail.com>
2017-04-29 21:56:13 +0400

    Init commit
    Create iOS application skeleton

---
+++ mobile/ios/Handy/Shared/Constants/HBAppColors.swift
@@ -0,0 +1,14 @@
+//
+//  HBAppColors.swift
+//  Handy
+//
+//  Created by Grigori Jlavyan on 4/29/17.
+//  Copyright © 2017 Handybook. All rights reserved.
+//
+
+import Foundation
+import UIKit
+struct AppColors {
+    static let GreenColor = UIColor(red: 139/256.0, green: 197/256.0, blue: 61/256.0, alpha: 1)
+    static let GrayColor = UIColor(red: 232/256.0, green: 232/256.0, blue: 232/256.0, alpha: 1)//E8E8E8
+}
