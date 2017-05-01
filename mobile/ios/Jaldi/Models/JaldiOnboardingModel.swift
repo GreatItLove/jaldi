599cce4978564aac86659bfd06c7a547ef8553db
Grigori Jlavyan <jlavyan@gmail.com>
2017-05-01 15:26:39 +0400

    Models

---
+++ mobile/ios/Jaldi/Models/JaldiOnboardingModel.swift
@@ -0,0 +1,14 @@
+//
+//  JaldiOnboardingModel.swift
+//  Jaldi
+//
+//  Created by Grigori Jlavyan on 4/30/17.
+//  Copyright © 2017 Handybook. All rights reserved.
+//
+
+import Foundation
+class JaldiOnboardingModel {
+    var zip:String?
+    var email:String?
+}
+
