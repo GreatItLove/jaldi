be714532a296813ee49051756ef3c57aae7d1f22
Grigori Jlavyan <jlavyan@gmail.com>
2017-05-01 18:30:12 +0400

    NotInYourAreaViewController

---
+++ mobile/ios/Jaldi/ViewControllerts/NotInYourAreaViewController/JaldiNotInYourAreaViewController.swift
@@ -0,0 +1,23 @@
+//
+//  JaldiNotInYourAreaViewController.swift
+//  Jaldi
+//
+//  Created by Grigori Jlavyan on 5/1/17.
+//  Copyright © 2017 Handybook. All rights reserved.
+//
+
+import UIKit
+
+class JaldiNotInYourAreaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    //MARK: Actions
+    @IBAction func backAction(_ sender: Any) {
+    }
+    @IBAction func tryAnotherZipAction(_ sender: Any) {
+    }
+}
