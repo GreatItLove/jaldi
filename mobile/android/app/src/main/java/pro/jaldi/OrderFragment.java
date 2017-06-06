af32890a6cb6058a7fd2e613f3fa155efb9067b2
Samvel Galstyan <samvel.galstyan.a@gmail.com>
2017-06-07 00:46:32 +0400

    Added missing files.

---
+++ mobile/android/app/src/main/java/pro/jaldi/OrderFragment.java
@@ -0,0 +1,222 @@
+package pro.jaldi;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A fragment representing a list of Items.
+ * <p/>
+ * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
+ * interface.
+ */
+public class OrderFragment extends Fragment {
+
+    public ArrayList<OrderModel> ordersArrayList = new ArrayList<>();
+    // TODO: Customize parameter argument names
+    private static final String ARG_COLUMN_COUNT = "column-count";
+    // TODO: Customize parameters
+    private int mColumnCount = 1;
+    private MyOrderRecyclerViewAdapter adapter;
+    private OnListFragmentInteractionListener mListener;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public OrderFragment() {
+    }
+
+    // TODO: Customize parameter initialization
+    @SuppressWarnings("unused")
+    public static OrderFragment newInstance(int columnCount) {
+        OrderFragment fragment = new OrderFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_COLUMN_COUNT, columnCount);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_order_list, container, false);
+
+        // Set the adapter
+        if (view instanceof RecyclerView) {
+            Context context = view.getContext();
+            RecyclerView recyclerView = (RecyclerView) view;
+            if (mColumnCount <= 1) {
+                recyclerView.setLayoutManager(new LinearLayoutManager(context));
+            } else {
+                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+            }
+            adapter = new MyOrderRecyclerViewAdapter(ordersArrayList, mListener);
+            recyclerView.setAdapter(adapter);
+        }
+        getOrders();
+        return view;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnListFragmentInteractionListener) {
+            mListener = (OnListFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnListFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnListFragmentInteractionListener {
+        void onListFragmentInteraction(OrderModel order);
+    }
+
+
+    private  void getOrders() {
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        String URL = "http://dev.jaldi.pro/rest/order/my";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.i("VOLLEY", response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("VOLLEY", error.toString());
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+
+                return getHeaderParams();
+            }
+
+            protected Map<String, String> getHeaderParams()
+            {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0MUBnbWFpbC5jb20iLCJyb2xlIjoiVVNFUiIsImV4cCI6MTU4MjYzNTU5MX0.jUCrG5MfmheOprwBcBJUhCYvgaNGooZhm_I__RnzqlAldwJF3aEzJ28AaY121vggakMHmS8HdE6lX1MCOhW4fA"); // admin@jaldi.pro
+                return params;
+            }
+
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                String responseString = "";
+                if (response != null) {
+                    String json = new String (response.data);
+                    Gson gson = new Gson();
+                    Type listType = new TypeToken<ArrayList<OrderModel>>(){}.getType();
+
+                    ordersArrayList = new Gson().fromJson(json, listType);
+
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            adapter.orderModels = ordersArrayList;
+                            adapter.notifyDataSetChanged();
+                        }
+                    });
+                }
+                return null;
+            }
+        };
+        requestQueue.add(stringRequest);
+    }
+    private void getOrder(int orderId) {
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        String URL = "http://dev.jaldi.pro/rest/order/" + orderId;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.i("VOLLEY", response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("VOLLEY", error.toString());
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+
+                return getHeaderParams();
+            }
+
+            protected Map<String, String> getHeaderParams()
+            {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0MUBnbWFpbC5jb20iLCJyb2xlIjoiVVNFUiIsImV4cCI6MTU4MjYzNTU5MX0.jUCrG5MfmheOprwBcBJUhCYvgaNGooZhm_I__RnzqlAldwJF3aEzJ28AaY121vggakMHmS8HdE6lX1MCOhW4fA"); // admin@jaldi.pro
+                return params;
+            }
+
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                String responseString = "";
+                if (response != null) {
+                    String json = new String (response.data);
+                    Gson gson = new Gson();
+                    OrderModel model = gson.fromJson(json, OrderModel.class);
+                    int a = model.id;
+                }
+                return null;
+            }
+        };
+        requestQueue.add(stringRequest);
+    }
+}
+
